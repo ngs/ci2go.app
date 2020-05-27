7927fe8ba074c5f7cc03ea9c2805bd99dae8d720
Atsushi Nagase <a@ngs.io>
2020-05-28 08:26:31 +0900

    Rename docs to public

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 set -eux
 
-aws s3 sync --acl public-read docs s3://ci2go-app-website
-aws s3 cp --acl public-read --content-type application/json docs/.well-known/apple-app-site-association s3://ci2go-app-website/docs/.well-known/apple-app-site-association
+aws s3 sync --acl public-read public s3://ci2go-app-website
+aws s3 cp --acl public-read --content-type application/json public/.well-known/apple-app-site-association s3://ci2go-app-website/docs/.well-known/apple-app-site-association
 
